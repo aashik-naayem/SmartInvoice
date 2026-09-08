f4c25092f924cfe904e9012f441d07bd2d604b22
Ashiiikkk <ashiknayem163@gmail.com>
2026-09-09 04:17:34 +0600

    Added PostMan

--- backend/src/main/java/com/smartinvoice/config/SecurityConfig.java
+++ backend/src/main/java/com/smartinvoice/config/SecurityConfig.java
@@ -31,8 +31,8 @@ public class SecurityConfig {
 
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(passwordEncoder());
-        provider.setUserDetailsService(userDetailsService);
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService); 
+        provider.setPasswordEncoder(passwordEncoder()); 
         return provider;
     }
 
